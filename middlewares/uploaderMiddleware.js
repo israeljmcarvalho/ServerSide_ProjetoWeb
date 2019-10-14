7760a0a314b6cce6d7e9a8b630685327ccbaf895
israeljmcarvalho <israeljmcarvalho@gmail.com>
2019-10-14 10:39:31 -0300

    Atualizado projeto até os 18min_55seg do vídeo Aula MiddleWare

---
+++ middlewares/uploaderMiddleware.js
@@ -0,0 +1,19 @@
+var multer = require ('multer');
+var path = require ('path'); // Path é uma biblioteca do Node para extrair extensão do arquivo  
+
+var storage = multer.diskStorage({
+  destination: function(res, file, cb) {
+      cb (null, 'public/images/');
+  },
+  filename:  function (req, file, cb){
+      var fileExt = path.extname(file.originalname)                 // Comando para extrair a extensão
+      var fileName = file.fieldname + '-' + Date.now() + fileExt    // Criei um novo nome do arquivo concatenando o nome original do arquivo file.fieldname
+                                                                    // com um traço e com a qdade de segundos que passou deste 1990 até o momento
+                                                                    // e com mais a extensão do arquivos fileExt
+      cb (null, this.fileName);
+  }
+}) 
+
+var upload = multer ({storage: storage});
+
+module.exports = upload; //Com este é um arquivo JS e todo arquivo JD dentro do Node deve ser exportado a funcionalidade
