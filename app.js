3ea4907c04cdc93dd913ae8d722c110204417e41
israeljmcarvalho <israeljmcarvalho@gmail.com>
2019-10-23 21:53:24 -0300

    Atualização Projeto Web

--- app.js
+++ app.js
@@ -3,13 +3,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminTestimonialsRouter = require ('./routes/admin/testimonials');
 var adminProjectsRouter = require ('./routes/admin/projects');
+var authRouter = require('./routes/auth');
+var verifyAuth = require('./middlewares/authMiddleware');
 
 var app = express();
+
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'mySecret123',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,6 +33,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
 app.use('/admin/testimonials', adminTestimonialsRouter);
 app.use('/admin/projects', adminProjectsRouter);
 app.use('/users', usersRouter);
